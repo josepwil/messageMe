882401eec1fe8ce6bb005022edd0c4c345e93380
Joseph Williams <josephwilliamsjs@gmail.com>
2021-05-21 11:47:14 -0400

    add auth system - create/destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,25 @@
 class SessionsController < ApplicationController
   def new
-    
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id 
+      flash[:success] = "Successfully logged in"
+      redirect_to root_path
+    else 
+      flash.now[:error] = "There was something wrong with your login information"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Succcessfully logged out"
+    redirect_to login_path
+  end
+  
+  
   
 end
